dd892528f47c82cf9791f8e37f39be20ff09bee5
Hasan Can <62356575+hasancankucuk@users.noreply.github.com>
2022-11-14 00:20:51 +0300

    karma unit test samples

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,7 +8,8 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +23,60 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-unit-test-sample'`, () => {
+  it('should be false if email is empty', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-unit-test-sample');
+    expect(app.emailFormControl.valid).toBeFalse();
   });
 
-  it('should render title', () => {
+  it('should be true if email form control is valid ', async () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-unit-test-sample app is running!');
+    const app = fixture.debugElement.componentInstance;
+    const el = fixture.nativeElement.querySelector('.email');
+    el.value = 'something';
+    el.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.emailFormControl.valid).toBeTrue();
+    });
+  });
+
+  it('should be false if password is empty', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.passwordFormControl.valid).toBeFalse();
+  });
+
+  it('should be false if password length is smaller then 6', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.debugElement.componentInstance;
+    const el = fixture.nativeElement.querySelector('.password');
+    el.value = '123';
+    el.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.passwordFormControl.valid).toBeFalse();
+    });
+  });
+
+  it('should be true if password form control is valid', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.debugElement.componentInstance;
+    const el = fixture.nativeElement.querySelector('.password');
+    el.value = '123456';
+    el.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.passwordFormControl.valid).toBeTrue();
+    });
+  });
+
+  it('should be true if button is disabled', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.isEnable).toBeFalse();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-unit-test-sample';
+  emailFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  isEnable: boolean = false;
+  ngOnInit() {
+    if(this.emailFormControl.valid && this.passwordFormControl.valid) {
+      this.isEnable = true;
+    }
+  }
+
+  isValid() {
+    if(this.emailFormControl.valid && this.passwordFormControl.valid) {
+      this.isEnable = true;
+    }
+  }
+  submit() {
+    console.log('TEST')
+    // alert('')
+  }
 }
